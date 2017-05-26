9e2ff5ea2eeac2d04919a5ef5513c9a120afc229
Jakub Dutkiewicz <kubadt@gmail.com>
2017-05-26 14:04:07 +0200

    Client projects

---
+++ W2VPClient/W2VPC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W2VPClient
+{
+    public class W2VPC
+    {
+
+    }
+}
