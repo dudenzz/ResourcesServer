8610ec82dd2b5923297f36b47069e04057afc714
Jakub Dutkiewicz <kubadt@gmail.com>
2017-06-07 13:34:56 +0200

    printing questions

--- QuestionBase.cs
+++ QuestionBase.cs
@@ -9,6 +9,14 @@ namespace ResourcesServerGit
 {
     public class Question
     {
+        public override string ToString()
+        {
+            string returnString = QuestionString + "\n";
+            for (int i = 0; i < possiblities.Length; i++)
+                returnString += "\t" + i.ToString() + ") " + possiblities[i] + "\n";
+            returnString += "Correct answer: " + correctAnswer;
+            return returnString;
+        }
         string questionString;
 
         public string QuestionString
@@ -59,15 +67,27 @@ namespace ResourcesServerGit
                         Regex qWordRE = new Regex(@".*\[(.*)\].*");
                         Regex pRE = new Regex(".*\\|([a-z]+)\\|([a-z]+)\\|([a-z]+)\\|([a-z]+)\\\".*");
                         Regex cRE = new Regex(".*:(.*)");
-                        Regex qRE = new Regex("\"(.*)\\.|");
+                        Regex qRE = new Regex("\"(.*\\.)\\|");
                         
                         if (qWordRE.Match(line).Groups.Count != 2) throw new Exception("more than one question word");
-                        Console.Write(qWordRE.Match(line).Groups[1].Value);
-                        Console.Write(pRE.Match(line).Groups.Count);
                         if (pRE.Match(line).Groups.Count != 5) throw new Exception("number of possibilities incorrect");
                         if (cRE.Match(line).Groups.Count != 2) throw new Exception("more than one correctAnswer");
                         if (qRE.Match(line).Groups.Count != 2) throw new Exception("more than one question");
 
+                        string qWord = qWordRE.Match(line).Groups[1].Value;
+                        string[] p = new string[] { 
+                            pRE.Match(line).Groups[1].Value, 
+                            pRE.Match(line).Groups[2].Value, 
+                            pRE.Match(line).Groups[3].Value, 
+                            pRE.Match(line).Groups[4].Value };
+                        string correct = cRE.Match(line).Groups[1].Value;
+                        string question = qRE.Match(line).Groups[1].Value;
+                        Question q = new Question();
+                        q.CorrectAnswer = correct;
+                        q.Possiblities = p;
+                        q.QuestionString = question;
+                        q.QuestionWord = qWord;
+                        Console.WriteLine(q);
                     }
                     break;
             }
